60b98401192f1af38906d0bdf9ddbd95b82ebdc4
Tanner Parkes <Tanner.lp9@gmail.com>
2024-02-20 12:50:47 -0800

    Changed and added new files

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,14 +1,15 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCBrrH_L7PCUCIFs_QYGHof8PYnfw1XQdc",
+    authDomain: "comp1800-57faf.firebaseapp.com",
+    projectId: "comp1800-57faf",
+    storageBucket: "comp1800-57faf.appspot.com",
+    messagingSenderId: "1071435250730",
+    appId: "1:1071435250730:web:829e6f4bee083eb433d753"
+  };
 
 //--------------------------------------------
 // initialize the Firebase app
